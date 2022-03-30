f9fe8a0f687491278bd0526efadec6985df3b72e
Jeremiah Gutherie <jgutherie@microsoft.com>
2022-03-30 10:24:23 -0400

    Now with  more Java

---
+++ JavaDependencyTest/src/test/java/com/advancesecuritydemo/dependencydemo/DependencydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advancesecuritydemo.dependencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
